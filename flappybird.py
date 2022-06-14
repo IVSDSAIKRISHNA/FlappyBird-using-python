bd3b0369ba3859b2a83f0e08209148eb93b252ab
IVSDSAIKRISHNA <ivsdsaikrishna1994@gmail.com>
2022-06-14 23:37:01 +0530

    +

--- flappybird.py
+++ flappybird.py
@@ -1,3 +1,13 @@
+import imp
 import random
 import sys
 import pygame
+from pygame.locals  import *
+
+FPS=32
+SCREENWIDTH=289
+SCREENHEIGHT=511
+SCREEN=pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
+GROUNDY= SCREENHEIGHT*0.8
+GAME_SPRITES ={}
+GAME_SOUNDS={}
