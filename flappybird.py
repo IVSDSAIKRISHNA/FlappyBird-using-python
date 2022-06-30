789d5858789594c13a034c95a782ae2d43b0fa2d
IVSDSAIKRISHNA <ivsdsaikrishna1994@gmail.com>
2022-07-01 00:00:37 +0530

    +

--- flappybird.py
+++ flappybird.py
@@ -52,7 +52,7 @@ def getRandomPipe():
     Generate posiitons of two pipes ( one bottom straight and one top rotated ) for blitting on the screen """ 
     pipeHeight= GAME_SPRITES['pipe'][0].get_height()
     offset = SCREENHEIGHT/3
-    yr = offset* random.randrange(0,int(SCREENHEIGHT-GAME_SPRITES['base'].get_height()-))
+    yr = offset* random.randrange(0,int(SCREENHEIGHT-GAME_SPRITES['base'].get_height()-1.2*offset))
 
 
 #Main Fucntions 
