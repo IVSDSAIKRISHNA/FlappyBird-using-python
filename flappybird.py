7a39102023e431d33fd57ed5c39265435b8a8018
IVSDSAIKRISHNA <ivsdsaikrishna1994@gmail.com>
2022-08-23 00:28:36 +0530

    +

--- flappybird.py
+++ flappybird.py
@@ -136,6 +136,21 @@ def mainGame():
         pygame.display.update()
         FPSCLOCK.tick(FPS)    
 def isCollide(playerx,playery,upperPipes,Lowerpipes):
+    if playery>GROUNDY-25 or playery<0:
+        GAME_SOUNDS['hit'].play()
+        return True
+    for pipe in upperPipes:
+        pipeHeight=GAME_SPRITES['pipe'][0].get_height()
+        if(playery<pipeHeight+pipe['y'] and abs(playerx-pipe['x']<GAME_SPRITES['pipe'][0].get_width())):
+            GAME_SOUNDS['hit'].play()
+            return True
+    for pipe in Lowerpipes:
+        if(playery+GAME_SPRITES['player'].get_height() > pipe['y']) and abs(playerx-pipe['x'])<GAME_SPRITES['pipe'][0].get_width():
+            GAME_SOUNDS['hit'].play()
+            return True
+    
+    
+    
     return False                   
 
 
